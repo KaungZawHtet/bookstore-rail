af3cffaebac2c0b5ce9cb89d7e560052b656eca0
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2024-03-12 12:25:37 +0700

    validation added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :pages, presence: true, numericality: { only_integer: true }
+  validates :description, presence: true, length: { minimum: 10 }
 end
