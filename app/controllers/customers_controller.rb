a4187bc912d19cfa12f4e2208a56c93abe3acfeb
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2024-03-12 15:37:38 +0700

    delete with dependency ok now

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,8 +2,15 @@ class CustomersController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @comment = @book.customers.create(customer_params)
+    @customer = @book.customers.create(customer_params)
     redirect_to book_path(@book)
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    @customer = @book.customers.find(params[:id])
+    @customer.destroy
+    redirect_to book_path(@book), status: :see_other
   end
 
   private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   include Visible
-  has_many :customers
+  has_many :customers, dependent: :destroy
   validates :title, presence: true
   validates :pages, presence: true, numericality: { only_integer: true }
   validates :description, presence: true, length: { minimum: 10 }
